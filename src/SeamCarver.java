1736f3423f03d60fadf855a2694c71561cf1e29d
Josh Mo <joshmomel@gmail.com>
2022-08-27 16:08:27 +1000

    feat: adding energy

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -1,29 +1,91 @@
 import edu.princeton.cs.algs4.Picture;
 
+import java.awt.Color;
+
 public class SeamCarver {
+
+  private final Picture picture;
+  private final double[][] energy;
+
 
   // create a seam carver object based on the given picture
   public SeamCarver(Picture picture) {
+    this.picture = new Picture(picture);
+    this.energy = new double[picture.width()][picture.height()];
   }
 
   // current picture
   public Picture picture() {
-    return null;
+    return this.picture;
   }
 
   // width of current picture
   public int width() {
-    return 0;
+    return this.picture.width();
   }
 
   // height of current picture
   public int height() {
-    return 0;
+    return this.picture.height();
+  }
+
+  private void validateRowIndex(int row) {
+    if (row < 0 || row >= height())
+      throw new IllegalArgumentException("row index must be between 0 and " + (height() - 1) + ": " + row);
+  }
+
+  private void validateColumnIndex(int col) {
+    if (col < 0 || col >= width())
+      throw new IllegalArgumentException("column index must be between 0 and " + (width() - 1) + ": " + col);
+  }
+
+  private boolean isBorder(int x, int y) {
+    return x == 0 || x == this.width() - 1 || y == 0 || y == this.height() - 1;
+  }
+
+  private int getDxSquare(int x, int y) {
+    Color leftColor = this.picture.get(x - 1, y);
+    Color rightColor = this.picture.get(x + 1, y);
+
+    int rx = Math.abs(leftColor.getRed() - rightColor.getRed());
+    int gx = Math.abs(leftColor.getGreen() - rightColor.getGreen());
+    int bx = Math.abs(leftColor.getBlue() - rightColor.getBlue());
+
+    return rx * rx + gx * gx + bx * bx;
+  }
+
+  private int getDySquare(int x, int y) {
+    Color upColor = this.picture.get(x, y - 1);
+    Color downColor = this.picture.get(x, y + 1);
+
+    int ry = Math.abs(upColor.getRed() - downColor.getRed());
+    int gy = Math.abs(upColor.getGreen() - downColor.getGreen());
+    int by = Math.abs(upColor.getBlue() - downColor.getBlue());
+
+
+    return ry * ry + gy * gy + by * by;
   }
 
   // energy of pixel at column x and row y
   public double energy(int x, int y) {
-    return 0;
+    validateColumnIndex(x);
+    validateRowIndex(y);
+
+    double energy = this.energy[x][y];
+    if (energy != 0.0d) {
+      return energy;
+    }
+
+
+    if (isBorder(x, y)) {
+      this.energy[x][y] = 1000;
+      return 1000;
+    }
+
+    double energyPixel = Math.sqrt(getDxSquare(x, y) + getDySquare(x, y));
+    this.energy[x][y] = energyPixel;
+
+    return energyPixel;
   }
 
   // sequence of indices for horizontal seam
@@ -46,6 +108,7 @@ public class SeamCarver {
 
   //  unit testing (optional)
   public static void main(String[] args) {
+    System.out.println(Math.sqrt(52024));
   }
 
 }

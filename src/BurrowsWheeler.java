a577e057c1d65cd5d65c97d582eec311f1496ddc
Josh Mo <joshmomel@gmail.com>
2022-09-20 16:16:22 +1000

    feat: finishing code

--- src/BurrowsWheeler.java
+++ src/BurrowsWheeler.java
@@ -1,7 +1,23 @@
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
+import edu.princeton.cs.algs4.SET;
+
+import java.util.Arrays;
+import java.util.TreeMap;
 
 public class BurrowsWheeler {
+
+
+  private static String sortString(String inputString) {
+    // Converting input string to character array
+    char[] tempArray = inputString.toCharArray();
+
+    // Sorting temp array using
+    Arrays.sort(tempArray);
+
+    // Returning new sorted string
+    return new String(tempArray);
+  }
 
   // apply Burrows-Wheeler transform,
   // reading from standard input and writing to standard output
@@ -28,13 +44,54 @@ public class BurrowsWheeler {
     }
 
     BinaryStdOut.close();
-
-
   }
 
   // apply Burrows-Wheeler inverse transform,
   // reading from standard input and writing to standard output
   public static void inverseTransform() {
+    String s = "";
+    BinaryStdIn.readInt();
+
+    while (!BinaryStdIn.isEmpty()) {
+      s = BinaryStdIn.readString();
+    }
+
+    String sortedString = sortString(s);
+
+    int N = s.length();
+    SET<Character> uniqueChars = new SET<>();
+    for (int i = 0; i < N; i++) {
+      uniqueChars.add(sortedString.charAt(i));
+    }
+
+    TreeMap<Character, Integer> count = new TreeMap<>();
+    int num = 0;
+    for (int i = 0; i < N; i++) {
+      char c = sortedString.charAt(i);
+      count.putIfAbsent(c, num);
+      num += 1;
+    }
+
+    int[] next = new int[N];
+    Arrays.fill(next, -1);
+
+    for (int i = 0; i < N; i++) {
+      char c = s.charAt(i);
+      int position = count.get(c);
+      while (next[position] != -1) {
+        position += 1;
+      }
+      next[position] = i;
+    }
+
+    int p = next[0];
+    for (int i = 0; i < N; i++) {
+      BinaryStdOut.write(sortedString.charAt(p));
+      p = next[p];
+    }
+
+
+    BinaryStdOut.close();
   }
 
   // if args[0] is "-", apply Burrows-Wheeler transform
